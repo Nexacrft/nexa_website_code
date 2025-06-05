7d1e083ce53ab1868e352ac71e210efc60e30ea7
Rohit tiwari <rt30082004@gmail.com>
2025-06-05 15:54:44 +0530

    changes

--- src/components/ExtraProjects.jsx
+++ src/components/ExtraProjects.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const ExtraProjects = () => {
   return (
-    <div>ExtraProjects</div>
+    <div className='p-10'>
+            <div className=" text-center flex flex-col lg:flex-row justify-between px-20 items-center">
+            <div>
+              <h1 className="monigue-font text-6xl">OUR</h1>
+              <h1 className="text-6xl md:text-9xl monigue-font text-[#F1F660]">
+                {" "}
+                PROJECTS
+              </h1>
+            </div>
+            <div>
+              <h1 className="text-3xl anton-sc-regular text-[#f4fada] hidden md:block">
+                &quot; Cool way to show projects &quot; <br /> ~ Sun Tzu
+              </h1>
+            </div>
+          </div>
+          <div>hello</div>
+    </div>
   )
 }
 
